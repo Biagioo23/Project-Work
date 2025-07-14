7aef0c72e47ee9ac93ecec954b52cf11a6a51dcb
Jacopo <jacopo.biagioni@itsrizzoli.it>
2025-07-14 10:46:05 +0200

    tab

--- streamlit/streamlit_1.py
+++ streamlit/streamlit_1.py
@@ -361,6 +361,7 @@ if st.session_state.logged_in:
 
         # Assegnazione Docenti - Corsi (DataFrame)
         df_unito = pd.merge(df_corso_docenti, df_corsi, on='idcorsoanno', how='inner')
+        df_selezionato = df_unito[['id_utente', 'cognome', 'nome', 'materia', 'corsi', 'monte_ore', 'ore_lavorate']]
         st.subheader("Assegnazione Docenti - Materia")
         if not df_corso_docenti.empty:
             st.dataframe(df_corso_docenti)
